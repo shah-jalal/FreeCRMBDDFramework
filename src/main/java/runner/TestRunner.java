e1c2d742dcf635b2c2166095d1c321c1290f407a
sjalal <sjalal0228@gmail.com>
2018-02-08 12:09:42 -0600

    add login steps

---
+++ src/main/java/runner/TestRunner.java
@@ -0,0 +1,30 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+	@RunWith(Cucumber.class)
+	@CucumberOptions(
+			features = "/Users/apple/gitProjects/FreeCRMBDDFramework/src/main/java/features", 
+			glue={"stepDefinitions"}, 
+			format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
+			monochrome = true, 
+			strict = true, 
+			dryRun = false 
+			//tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}			
+			)
+	 
+	public class TestRunner {
+	 
+}
+
+/*Note: 
+ * features = "the path of the feature files"
+ * glue={"the path of stepDefinitions"}
+ * format= {to generate different types of reporting}
+ * monochrome = display the console output in a proper readable format
+ * strict = it will check if any step is not defined in step definition file
+ * dryRun = to check the mapping is proper between feature file and step_def file
+ */
---
+++ test-outout/report.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Free CRM Login Feature",
+  "description": "",
+  "id": "free-crm-login-feature",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "comments": [
+    {
+      "line": 4,
+      "value": "#without Examples Keyword"
+    }
+  ],
+  "line": 5,
+  "name": "Free CRM Login Test Scenario",
+  "description": "",
+  "id": "free-crm-login-feature;free-crm-login-test-scenario",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 7,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "user enters \"naveenk\" and \"test@123\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_already_on_Login_Page()"
+});
+formatter.result({
+  "duration": 103017268,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.title_of_login_page_is_Free_CRM()"
+});
+formatter.result({
+  "duration": 26559,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "naveenk",
+      "offset": 13
+    },
+    {
+      "val": "test@123",
+      "offset": 27
+    }
+  ],
+  "location": "LoginStepDefinition.user_enters_and(String,String)"
+});
+formatter.result({
+  "duration": 2037063,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_clicks_on_login_button()"
+});
+formatter.result({
+  "duration": 21943,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_home_page()"
+});
+formatter.result({
+  "duration": 19565,
+  "status": "passed"
+});
+});
